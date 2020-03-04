4d13ef049ac2e21f0a5ccb7f4cab0ac23f150ec7
G0ldenPh3onix <60448742+G0ldenPh3onix@users.noreply.github.com>
2020-03-03 17:26:34 -0700

    Code for Enemy Projectiles

---
+++ EnemyProjectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public GameObject enemyBullet;
+    public GameObject player;
+    private GameObject projectile;
+    
+
+    public float projectileSpeed = 30;
+
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    void EnemyFire()
+    {
+        Instantiate(enemyBullet, transform.position, transform.rotation);
+        enemyBullet.GetComponent<Rigidbody>().AddForce(0, 0, 5);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+
+            SendMessage("ChangeHealth", -10);
+        }
+    }
+}
