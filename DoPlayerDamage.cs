eee765d9f95a9ca5b3ecf22444ebd84ae4d4dd40
G0ldenPh3onix <60448742+G0ldenPh3onix@users.noreply.github.com>
2020-03-03 17:25:52 -0700

    Code to deal damage to health

---
+++ DoPlayerDamage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DoPlayerDamage : MonoBehaviour
+{
+    public Slider healthFill;
+
+    public float currentHealth;
+    public float maxHealth = 100;
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown("j"))
+        {
+            ChangeHealth(-10);
+        }
+
+        if(currentHealth <= 0)
+        {
+            SendMessage("PlayerDied");
+        }
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        healthFill.value = currentHealth / maxHealth;
+    }
+}
